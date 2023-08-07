676300f825193c60a8d58d0fb16d354eb288910f
ks6088ts <ks6088ts@gmail.com>
2023-08-07 16:35:28 +0900

    set default values

--- api/settings/azure_cognitive_search.py
+++ api/settings/azure_cognitive_search.py
@@ -2,9 +2,9 @@ from pydantic import BaseSettings
 
 
 class AzureCognitiveSearchSettings(BaseSettings):
-    service_name: str
-    index_name: str
-    api_key: str
+    service_name = "YOUR_SERVICE_NAME"
+    index_name = "YOUR_INDEX_NAME"
+    api_key = "YOUR_API_KEY"
 
     class Config:
         env_file = "azure_cognitive_search.env"
--- api/settings/chat.py
+++ api/settings/chat.py
@@ -5,7 +5,7 @@ class ChatSettings(BaseSettings):
     openai_model_name = "gpt-35-turbo"
     openai_deployment_id = "chat"
     openai_api_type = "azure"
-    openai_api_key: str
+    openai_api_key = "YOUR_API_KEY"
     openai_api_base = "https://AOAI_NAME.openai.azure.com/"
     openai_api_version = "2023-07-01-preview"
 
