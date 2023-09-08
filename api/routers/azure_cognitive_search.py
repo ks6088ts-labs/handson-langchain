37b7dd3218fa34d2a81964eeb8e79f90925ca789
ks6088ts <ks6088ts@gmail.com>
2023-09-08 12:23:08 +0900

    avoid creating vector store when booting server

--- api/routers/azure_cognitive_search.py
+++ api/routers/azure_cognitive_search.py
@@ -10,21 +10,28 @@ from api.settings import azure_cognitive_search as azure_cognitive_search_settin
 
 settings = azure_cognitive_search_settings.AzureCognitiveSearchSettings()
 router = APIRouter()
-embeddings: OpenAIEmbeddings = OpenAIEmbeddings(
-    model=settings.openai_model_name,
-    deployment=settings.openai_deployment_id,
-    openai_api_version=settings.openai_api_version,
-    openai_api_base=settings.openai_api_base,
-    openai_api_type=settings.openai_api_type,
-    openai_api_key=settings.openai_api_key,
-    chunk_size=1,
-)
-vector_store: AzureSearch = AzureSearch(
-    azure_search_endpoint=settings.azure_cognitive_search_endpoint,
-    azure_search_key=settings.azure_cognitive_search_key,
-    index_name=settings.azure_cognitive_search_index_name,
-    embedding_function=embeddings.embed_query,
-)
+vector_store = None
+
+
+def get_vector_store() -> AzureSearch:
+    if vector_store is not None:
+        return vector_store
+
+    embeddings: OpenAIEmbeddings = OpenAIEmbeddings(
+        model=settings.openai_model_name,
+        deployment=settings.openai_deployment_id,
+        openai_api_version=settings.openai_api_version,
+        openai_api_base=settings.openai_api_base,
+        openai_api_type=settings.openai_api_type,
+        openai_api_key=settings.openai_api_key,
+        chunk_size=1,
+    )
+    return AzureSearch(
+        azure_search_endpoint=settings.azure_cognitive_search_endpoint,
+        azure_search_key=settings.azure_cognitive_search_key,
+        index_name=settings.azure_cognitive_search_index_name,
+        embedding_function=embeddings.embed_query,
+    )
 
 
 @router.post(
@@ -34,6 +41,7 @@ vector_store: AzureSearch = AzureSearch(
 async def search_index(
     request: azure_cognitive_search_schemas.SearchRequest,
 ):
+    vector_store = get_vector_store()
     docs = vector_store.similarity_search(
         query=request.query,
         k=3,
