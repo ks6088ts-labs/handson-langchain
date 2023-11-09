9da66839b1e9d069582cb3bb2f0293f3a9ea3683
ks6088ts <ks6088ts@gmail.com>
2023-11-09 14:31:08 +0900

    add review usecase

--- scripts/code.py
+++ scripts/code.py
@@ -11,12 +11,15 @@ app = typer.Typer()
 
 @app.command()
 def run(
+    prompt: str,
     env_file: str = "./notebooks/credentials.env",
-    prompt: str = "あなたは python で書かれたプログラムコードからテストフレームワーク pytest を使ったテストコードを生成するのに役立つ Microsoft のソフトウェアエンジニアです。以下のプログラムコードに対応するテストコードを作成し、test_generated.py として保存してください。\\n```python\\n def add(a: int, b: int):\\n  return a + b```",
 ):
     """
-    To run the pipeline, you need to provide a path to the dataset and the output directory
-        > poetry run python scripts/generate_test_code.py --env-file "./notebooks/credentials.env" --prompt '"あなたは python で書かれたプログラムコードからテストフレームワーク pytest を使ったテストコードを生成するのに役立つ Microsoft のソフトウェアエンジニアです。以下のプログラムコードに対応するテストコードを作成し、test_generated.py として保存してください。\\n```python\\n def add(a: int, b: int):\\n  return a + b```"'
+    Use cases:
+        Test code generation: Generate test code from a given code snippet
+            > poetry run python scripts/code.py --env-file "./notebooks/credentials.env" --prompt '"あなたは python で書かれたプログラムコードからテストフレームワーク pytest を使ったテストコードを生成するのに役立つ Microsoft のソフトウェアエンジニアです。以下のプログラムコードに対応するテストコードを作成し、test_generated.py として保存してください。\\n```python\\n def add(a: int, b: int):\\n  return a + b```"'
+        Test code generation: Review codes passed from prompt
+            > poetry run python scripts/code.py --env-file "./notebooks/credentials.env" --prompt '"あなたは Microsoft のソフトウェアエンジニアです。以下のプログラムコードをレビューし、Reviewdog Diagnostic Format に沿ったフォーマットでレビュー結果を result.rdjson として保存してください。\\n```python\\n def add(a: int, b: int):\\n  return a - b```"'
     """
     typer.echo(f"Running pipeline with {env_file}, {prompt}")
     load_dotenv(env_file)
