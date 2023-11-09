68e7e4414de578f5b7f71448b285168e46d87ca0
ks6088ts <ks6088ts@gmail.com>
2023-11-09 14:44:14 +0900

    bugfix for prompt option

--- scripts/code.py
+++ scripts/code.py
@@ -11,7 +11,7 @@ app = typer.Typer()
 
 @app.command()
 def run(
-    prompt: str,
+    prompt: str = None,
     env_file: str = "./notebooks/credentials.env",
 ):
     """
@@ -21,6 +21,10 @@ def run(
         Test code generation: Review codes passed from prompt
             > poetry run python scripts/code.py --env-file "./notebooks/credentials.env" --prompt '"あなたは Microsoft のソフトウェアエンジニアです。以下のプログラムコードをレビューし、Reviewdog Diagnostic Format に沿ったフォーマットでレビュー結果を result.rdjson として保存してください。\\n```python\\n def add(a: int, b: int):\\n  return a - b```"'
     """
+    if prompt is None:
+        _prompt = typer.prompt("Please input prompt")
+        prompt = f'"{_prompt}"'
+
     typer.echo(f"Running pipeline with {env_file}, {prompt}")
     load_dotenv(env_file)
 
