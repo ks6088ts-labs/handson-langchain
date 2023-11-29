fbaea34d8f53a98f74b97ecb34936503fd344022
ks6088ts <ks6088ts@gmail.com>
2023-11-29 10:31:31 +0900

    use text splitter to support large docs

--- api/routers/youtube.py
+++ api/routers/youtube.py
@@ -1,9 +1,13 @@
+from typing import List
+
 from fastapi import APIRouter
 from langchain.callbacks import get_openai_callback
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
+from langchain.docstore.document import Document
 from langchain.document_loaders import YoutubeLoader
 from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from api.schemas import youtube as youtube_schemas
 from api.settings import youtube as youtube_settings
@@ -36,29 +40,54 @@ def create_llm():
     )
 
 
-def get_documents(url):
-    return YoutubeLoader.from_youtube_url(
+def get_documents(url: str) -> str:
+    docs = YoutubeLoader.from_youtube_url(
         url, add_video_info=True, language=["en", "ja"]
     ).load()
+    if len(docs) == 0:
+        raise ValueError("No documents found.")
+    return docs[0].page_content
 
 
-def get_summary(llm, docs):
+def split_docs(text: str) -> List[Document]:
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", " ", "", "。", "!", "！", "?", "？"],
+        chunk_size=15000,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
+
+    texts = text_splitter.split_text(text)
+    print(f"splitted to {len(texts)} chunks")
+    return [Document(page_content=t) for t in texts]
+
+
+def summarize(llm: ChatOpenAI, docs: [Document]):
+    prompt = PromptTemplate(template=PROMPT_TEMPLATE, input_variables=["text"])
     with get_openai_callback() as cb:
         chain = load_summarize_chain(
-            llm,
-            chain_type="stuff",
+            llm=llm,
+            chain_type="map_reduce",
+            map_prompt=prompt,
+            combine_prompt=prompt,
+            collapse_prompt=prompt,
             verbose=False,
-            prompt=PromptTemplate(template=PROMPT_TEMPLATE, input_variables=["text"]),
         )
-        response = chain({"input_documents": docs}, return_only_outputs=True)
-    return response["output_text"], cb.total_cost
+
+        output = chain(
+            inputs=docs,
+            return_only_outputs=True,
+        )["output_text"]
+    return output, cb.total_cost
 
 
 @router.post("/youtube/summarize", response_model=youtube_schemas.SummarizeResponse)
 async def chat(body: youtube_schemas.SummarizeRequest):
     llm = create_llm()
-    docs = get_documents(body.url)
-    summary, cost = get_summary(llm, docs)
+    transcript = get_documents(body.url)
+    docs = split_docs(transcript)
+    summary, cost = summarize(llm, docs)
 
     return youtube_schemas.SummarizeResponse(
         content=summary,
